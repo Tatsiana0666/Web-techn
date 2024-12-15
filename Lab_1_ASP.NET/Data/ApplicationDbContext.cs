48e3f6a1f724789a1b3d6d109f2c0356d5acf47c
Tatsiana0666 <afina.pallada666@gmail.com>
2024-12-15 20:32:31 +0300

    Web-techn
    Web-techn

---
+++ Lab_1_ASP.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Lab_1_ASP.NET.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab_1_ASP.NET.Data {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+    }
+}
